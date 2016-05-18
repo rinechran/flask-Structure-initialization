72c611187ec2e58341b57a54f6e9f242b9b02a0a
Jung Eun Sik <rinechran@outlook.jp>
2016-05-19 05:40:35 +0900

    Bug fix
    Bug fix

--- application/__init__.py
+++ application/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__,static_folder=None)
-db = SQLAlchemy(app)
 
 from application.config import *
 app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+
 
 
 from application.main import main_buleprint
